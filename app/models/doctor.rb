232ebc1ded13eb27e6b94bc7f7239283def26420
Tamara Jost <facebookoftjo@gmail.com>
2017-04-25 17:54:11 -0300

    doctor index

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,8 +1,16 @@
 class Doctor < ApplicationRecord
-  belongs_to :user
-  belongs_to :review
+  belongs_to :user, optional:true
+  has_many :reviews
   has_many :treatmentspecialties
   has_many :specialties, through: :treatmentspecialties
+
+  def average_review
+    if self.reviews.size > 0
+        self.reviews.average(:Rating)
+    else
+        "-"
+    end
+  end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :users, only: [:show]
   resources :reviews
+  resources :doctors
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
