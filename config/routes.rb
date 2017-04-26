a4641a9ad83ec11829f03c8a947192e67e9340ea
pedro almeida <pedro.marc2003@gmail.com>
2017-04-26 14:50:20 -0300

    controller create + view do modal para review

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :users, only: [:show]
-  resources :reviews
-  resources :doctors
-
+  resources :doctors do
+    resources :reviews, only: [:create, :destroy]
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
