cd488b2fda51a78cb5e8beef7067b4d8fe885d27
Tamara Jost <facebookoftjo@gmail.com>
2017-04-27 18:13:41 -0300

    see doctors witouth login

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,7 @@
 class DoctorsController < ApplicationController
 
   before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @search = Search.new(search_params)
