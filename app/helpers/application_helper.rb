4747299e9079182792ffa313582766bfc29b03f6
pedro almeida <pedro.marc2003@gmail.com>
2017-04-28 14:03:38 -0300

    nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def avatar_url
       if current_user.nil?
         "http://placehold.it/30x30"
+      elsif current_user.facebook_picture_url.nil?
+        "http://placehold.it/30x30"
       else
         current_user.facebook_picture_url
       end
