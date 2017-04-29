bb504e245aa2ffe81fc9621a05c45e164eb6c883
pedro almeida <pedro.marc2003@gmail.com>
2017-04-29 20:48:12 -0300

    links feitos para integracao de perfis

--- app/controllers/doctormatches_controller.rb
+++ app/controllers/doctormatches_controller.rb
@@ -6,6 +6,10 @@ class DoctormatchesController < ApplicationController
 
   def status
     @doctor = Doctor.find(params[:id])
+    current_user.doctorstatus = "pendding"
+    current_user.save
+    @doctor.user_id = current_user.id
+    @doctor.save
     redirect_to doctor_path
     flash[:notice] = "We'll Contact you to verify your information shortly " + @doctor.name
   end
