f34d59537ef31e70a1c08ec7763020df3a7346b1
Tamara Jost <facebookoftjo@gmail.com>
2017-05-02 11:59:41 -0300

    login window

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :about]
 
   def home
     @reviews_first = Review.first
     @reviews_second = Review.second
     @reviews_third = Review.third
+  end
+
+  def about
   end
 end
 
